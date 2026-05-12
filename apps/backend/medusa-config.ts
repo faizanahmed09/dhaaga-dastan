908a8f1138da6c2c5cba66c6bea0206fa5fb62ce
faizanahmed09 <raofaizan09@gmail.com>
2026-05-12 11:49:52 +0500

    feat: update Medusa configuration to use environment variables for CORS settings

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -6,15 +6,10 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
-    cookieOptions: {
-      sameSite: "none",
-      secure: true,
-      httpOnly: true,
-    },
     http: {
-      storeCors: "/.*/",
-      adminCors: "/.*/",
-      authCors: "/.*/",
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
